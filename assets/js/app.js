c5f4541f179234c9e4efc328d7d38d589b2d20ca
pilarfc <pilar.fc@outlook.com>
2017-06-06 15:53:43 +0000

    version 4 finalizada

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,31 @@
-// Funcionalidad mapa y marcador
-function initMap() {
-  var coordenadas = {lat:19.4176387, lng:-99.16481529999999};
+// Ubicación Actual
 
-  var map = new google.maps.Map(document.getElementById('mapa'), {
+function obtenerUbicacionActual() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarPosicionActual);
+  } else {
+    alert("La Geolocalización no funciona en tu navegador.");
+  }
+}
+
+
+function mostrarPosicionActual(posicion) {
+  var latitud = posicion.coords.latitude;
+  var longitud = posicion.coords.longitude;
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  initMap(coordenadas);
+}
+
+
+// Funcionalidad mapa y marcador
+function initMap(coordenadas) {
+    
+  var map = new google.maps.Map($('#mapa')[0], {
     zoom: 18,
     center: coordenadas
   });
@@ -18,7 +41,7 @@ function initMap() {
 var lugares = [
     {
         "nombre": "Pizza del Perro Negro",
-        "coordenadas":  {"lat":"19.417091", "lng":"-99.169670"},
+        "coordenadas":  {"lat":19.417091, "lng":-99.169670},
         "direccion": "Parque España 3, Roma Nte., 06700 Ciudad de México, CDMX.",
         "numero":  "01 55 5351 7401",
         "logo": "assets/img/perronegro.png" 
@@ -26,7 +49,7 @@ var lugares = [
     
     {
         "nombre": "Mercado Roma",
-        "coordenadas":  {"lat":"19.414133", "lng":"-99.164237"},
+        "coordenadas":  {"lat":19.414133, "lng":-99.164237},
         "direccion": "Calle Querétaro 225, Roma Nte., 06700 Ciudad de México, CDMX.",
         "numero":  "01 55 5564 1396",
         "logo": "assets/img/mercadoroma.jpg" 
@@ -34,7 +57,7 @@ var lugares = [
     
     {
         "nombre": "Papa Guapa",
-        "coordenadas":  {"lat":"19.419000", "lng":"-99.166930"},
+        "coordenadas":  {"lat":19.419000, "lng":-99.166930},
         "direccion": "Av Oaxaca 80, Cuauhtemoc, Roma Norte, Roma Nte., 06700 Ciudad de México, CDMX.",
         "numero":  " 01 55 5207 8052",
         "logo": "assets/img/papaguapa.png" 
@@ -42,7 +65,7 @@ var lugares = [
     
     {
         "nombre": "Barracuda Diner",
-        "coordenadas":  {"lat":"19.420042", "lng":"-99.166855"},
+        "coordenadas":  {"lat":19.420042, "lng":-99.166855},
         "direccion": "Av Nuevo León 4-A, Condesa, Hipódromo, 06140 Cuauhtémoc, CDMX.",
         "numero":  "01 55 5211 9480",
         "logo": "assets/img/barracudadiner.jpg" 
@@ -50,7 +73,7 @@ var lugares = [
     
     {
         "nombre": "Helado Obscuro",
-        "coordenadas":  {"lat":"19.412826", "lng":"-99.158488"},
+        "coordenadas":  {"lat":19.412826, "lng":-99.158488},
         "direccion": "Orizaba 203, Roma Nte., 06700 Ciudad de México, CDMX.",
         "numero":  "01 55 4444 4878",
         "logo": "assets/img/heladoobscuro.jpg" 
@@ -59,13 +82,13 @@ var lugares = [
 
 
 var plantillaLugar =  '<div class="col s12 m7">' +
-        '<div class="card horizontal">' +
-          '<div class="card-image">' +
+        '<div class="card horizontal cursor">' +
+          '<div class="card-image">' + 
             '<img src="__logo__">' +
           '</div>' +
           '<div class="card-stacked">' +
             '<div class="card-content">' +
-              '<h4>__nombre__</h4>' +
+              '<h4 class="restaurante" data-latitud="__latitud__" data-longitud="__longitud__">__nombre__</h4>' +
               '<p>__direccion__</p>' +
               '<p class="texto-tarjeta-telefono"><i class="material-icons">phone</i> __numero__</p>' +
             '</div>' +
@@ -80,7 +103,10 @@ var crearTarjetas = function (lugares) {
 		plantillaFinal += plantillaLugar.replace("__nombre__", restaurante.nombre)
             .replace("__logo__", restaurante.logo)
 			.replace("__numero__", restaurante.numero)
-			.replace("__direccion__", restaurante.direccion);
+			.replace("__direccion__", restaurante.direccion)
+            .replace("__latitud__", restaurante.coordenadas.lat)
+            .replace("__longitud__", restaurante.coordenadas.lng);
+        
 	});
 	$(".listaDeRestaurantes").html(plantillaFinal);
 };
@@ -96,10 +122,26 @@ var filtrarRestaurantes = function (e) {
 };
 
 
+function cambiarUbicacion() {
+    
+  var latitud =  $(this).data("latitud");
+  var longitud = $(this).data("longitud");
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  initMap(coordenadas);
+}
+
+
 var cargarPagina = function () {
     
    crearTarjetas(lugares); 
+   obtenerUbicacionActual();
    $("#search-form").submit(filtrarRestaurantes);
+   $(".restaurante").click(cambiarUbicacion);
 }
 
 
